83d6720800d73b7aa65ccc9037a2774c3b0528a6
m.ehab2 <muhamedehab1@gmail.com>
2023-03-26 05:59:42 +0200

    ain

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 let screen=document.getElementById("Screen");
 function append(n){
-    console.log(n);
-    screen.innerHTML=`${n}`;
+    if(screen.innerHTML.length>12){
+        return 0;
+    }
+    if(screen.innerHTML==="0"){
+        screen.innerHTML=`${n}`;
+    }
+    else{
+        screen.innerHTML=screen.innerHTML+`${n}`;
+    }
+}
+function op(m){
+    if (m===`bcksp`) {
+        if (screen.innerHTML.length>1) {
+            let s=screen.innerHTML.slice(0,screen.innerHTML.length-1)
+            screen.innerHTML=s; 
+        }
+        else{
+            screen.innerHTML=`0`;
+        }
+    }
+    
 }
